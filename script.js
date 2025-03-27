9dc8c7b8623026768bfda744d221f365f157a478
Harshita <itsharshita0201@gmail.com>
2025-03-27 17:00:26 +0530

    completed

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+ // locomotive scrollTrigger codepan cdn
+ function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    
+      // for tablet smooth
+      tablet: { smooth: true },
+    
+      // for mobile
+      smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        };
+      }
+    });
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    ScrollTrigger.refresh();
+    
+}
+function loadingAnimation() {
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "150px",
+        duration: 1.35,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+}
+
+
+
 function navAnimation() {
     var nav = document.querySelector("nav")
 
@@ -102,13 +165,51 @@ sections.forEach(function (elem) {
     })
 })
 }
+function page6Animations() {
+    gsap.from("#btm6-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            // markers:true,
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+    gsap.from("#btm6-part3 h4", {
+        x: 0,
+        duration:0.8,
+        scrollTrigger: {
+            trigger: "#btm6-part3",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+    gsap.from("#btm6-part4 h4", {
+        x: 0,
+        duration: 0.8,
+        scrollTrigger: {
+            trigger: "#btm6-part4",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+}
 
-
-
-
-
-
+locomotiveAnimation()
 
 navAnimation()
+
 page2Animation()
+
 page3VideoAnimation()
+
+page6Animations()
+
+loadingAnimation()
